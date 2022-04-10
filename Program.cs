fd94212f88bbac373f2236134f766cf240b1c89f
= <=>
2022-04-10 19:10:26 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double fgts, salario;
+
+Console.Write("Salário (R$):");
+salario = Convert.ToDouble (Console.ReadLine ());
+
+fgts = (8 * salario) / 100 ;
+
+Console.WriteLine($"\nFGTS......:R$ {fgts:N2}");
